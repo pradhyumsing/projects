a26d8ec67a0c7d03f485ee5d1d41792473ec5958
pradhyum patel <138298198+pradhyumsing@users.noreply.github.com>
2023-07-27 13:42:12 +0530

    Update settings.py

--- iportf/settings.py
+++ iportf/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = ')g%houo@vf&$*c$-r6pe#e=cmy_d$q1gj-!2(qo-89o-%9oc=4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -117,9 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     
 ]
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build', 'static')
